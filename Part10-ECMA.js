8a6f2e1b360defaed2f1df3ce015d68442c8e7cf
POOVESH ANAND <deveshlive2912@gmail.com>
2021-09-30 17:42:00 +0530

    DOM vs Window

---
+++ Part10-ECMA.js
@@ -0,0 +1,3 @@
+//Windows vs DOM : Window is the main container or we can say the global object and any operation related to entire browser window can be a part of window object
+//DOM is the child of window object . So url is the part of window not DOM
+ 
